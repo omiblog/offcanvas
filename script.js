deb22d2ac8546f73a98661d9a0eca12fca656da4
omblog <omprakshpal739@gmail.com>
2022-06-07 10:12:01 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const showMenu = () => {
+    const m = document.querySelector('.menu');
+    m.classList.add('smenu');
+}
+const closeMenu = () => {
+    const n = document.querySelector('.menu');
+    n.classList.remove('smenu');
+}
+
